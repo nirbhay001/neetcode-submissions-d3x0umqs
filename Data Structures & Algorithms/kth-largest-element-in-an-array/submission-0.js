3bcf006539bc1c7f8b292075bca991ff35a8aec9
nirbhay001 <65802900+nirbhay001@users.noreply.github.com>
2026-05-08 01:13:41 +0530

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.js
@@ -0,0 +1,91 @@
+
+class meanHeap{
+    constructor(){
+        this.heap = [];
+    }
+
+    getParent(i){
+        return Math.floor((i-1)/2);
+    }
+    getLeft(i){
+        return Math.floor(2*i) + 1;
+    }
+    getRight(i){
+        return Math.floor(2*i) + 2;
+    }
+    swap(first, second){
+        [this.heap[first], this.heap[second]] = [this.heap[second], this.heap[first]]
+    }
+    insert(element){
+        this.heap.push(element);
+        this.heapifyUp();
+
+    }
+    heapifyUp(){
+        let l = this.heap.length-1;
+        while(true){
+            let parent = this.getParent(l);
+            if(this.heap[parent] < this.heap[l]){
+                [this.heap[parent], this.heap[l]] = [this.heap[l], this.heap[parent]];
+                l = parent;
+            } else {
+                break;
+            }
+
+        }
+    }
+    remove(){
+        if(this.heap.length === 1){
+            return this.heap.pop();
+        }
+        let max = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return max;
+    }
+    heapifyDown(){
+        let i = 0;
+        while(true){
+            let left = this.getLeft(i);
+            let right = this.getRight(i);
+            let smallest = i;
+            let lengthD = this.heap.length
+            if(left < lengthD && this.heap[left] > this.heap[smallest]){
+                smallest = left;
+            }
+            if(right < lengthD && this.heap[right] > this.heap[smallest]){
+                smallest = right;
+            }
+            if (smallest === i) {
+                break;
+            }
+            [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+            i = smallest
+        }
+    };
+    size() {
+        return this.heap.length;
+    };
+}
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        let heap = new meanHeap();
+        let largest = 0;
+        for(let i=0;i<nums.length;i++){
+            heap.insert(nums[i]);
+        }
+        while(k > 0){
+            let high = heap.remove();
+            largest = high;
+            k--;
+        }
+        return largest;
+
+    }
+}
